291fc1d582400f5b7f215d65a766e60c2359a22a
Dawson Botsford <dawsonb@uber.com>
2017-01-20 13:04:22 -0800

    test existing 2016 header. Closes #19

--- test/fixtures/add-header-none/input.js
+++
@@ -1,2 +0,0 @@
-
-Example file content
--- test/index.js
+++ test/index.js
@@ -1,42 +1,63 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 var tape = require('tape');
 var path = require('path');
 var fs = require('fs-extra');
 var temp = require('temp');
 var exec = require('child_process').exec;
-var escape = require('escape-string-regexp');
+var esc = require('escape-string-regexp');
 
 // Automatically track and cleanup files at exit
 temp.track();
 
 var uberLicensePath = path.join(__dirname, '..', 'bin', 'licence.js');
 
-tape('add-header-none', function (t) {
-	var fixturePath = path.join(__dirname, 'fixtures', 'add-header-none');
-	var inputFilePath = path.join(fixturePath, 'input.js');
+tape('add-header', function (t) {
+	t.plan(2);
+	var fixturePath = path.join(__dirname, 'fixtures', 'add-header');
+	var input1FilePath = path.join(fixturePath, 'input-1.js');
+	var input2FilePath = path.join(fixturePath, 'input-2.js');
 	var outputFileContent = fs.readFileSync(path.join(fixturePath, 'output.js'), 'utf8');
 
 	// remove the dynamic year portion of the header comment
 	var consistantOutputString = outputFileContent.substring(outputFileContent.indexOf('Permission'));
-	var outputRegex = new RegExp(escape(consistantOutputString));
+	var outputRegex = new RegExp(esc(consistantOutputString));
 
-	temp.mkdir('add-header-none', function(mkTempErr, dirPath) {
+	temp.mkdir('add-header', function(mkTempErr, dirPath) {
 	  if (mkTempErr) {
 	  	return console.error('mkTempErr: ' + mkTempErr);
 	  }
-		fs.copy(inputFilePath, path.join(dirPath, 'input.js'), function (copyErr) {
-		  if (copyErr) {
-		  	return console.error('copyErr: ' + copyErr);
-		  }
-		  process.chdir(dirPath);
-		  exec(uberLicensePath + ' ./input.js', function(execErr) {
-		  	if (execErr instanceof Error) {
-		  		throw execErr;
-		  	}
-				var inputFileContent = fs.readFileSync('input.js', 'utf8');
-				// delete until newline in outputRegex
-				t.true(outputRegex.test(inputFileContent), 'should prepend header');
-				t.end();
-		  });
+	  process.chdir(dirPath);
+		fs.copySync(input1FilePath, 'input-1.js');
+		fs.copySync(input2FilePath, 'input-2.js');
+	  exec(uberLicensePath, function(execErr) {
+	  	if (execErr instanceof Error) {
+	  		throw execErr;
+	  	}
+			var input1FileContent = fs.readFileSync('input-1.js', 'utf8');
+			var input2FileContent = fs.readFileSync('input-2.js', 'utf8');
+
+			t.true(outputRegex.test(input1FileContent), 'should prepend header to file without header');
+			t.true(outputRegex.test(input2FileContent), 'should replace old header (from 2016)');
+			t.end();
 		});
 	});
 });
@@ -49,26 +70,21 @@ tape('dont-add-header', function (t) {
 
 	// remove the dynamic year portion of the header comment
 	var consistantOutputString = outputFileContent.substring(outputFileContent.indexOf('Permission'));
-	var outputRegex = new RegExp(escape(consistantOutputString));
+	var outputRegex = new RegExp(esc(consistantOutputString));
 
 	temp.mkdir('dont-add-header', function(mkTempErr, dirPath) {
 	  if (mkTempErr) {
 	  	return console.error('mkTempErr: ' + mkTempErr);
 	  }
-		fs.copy(inputFilePath, path.join(dirPath, 'input.js'), function (copyErr) {
-		  if (copyErr) {
-		  	return console.error('copyErr: ' + copyErr);
-		  }
-		  process.chdir(dirPath);
-			var command = uberLicensePath;
-		  exec(command, function(execErr) {
-		  	if (execErr instanceof Error) {
-		  		throw execErr;
-		  	}
-				var inputFileContent = fs.readFileSync('input.js', 'utf8');
-				t.true(outputRegex.test(inputFileContent), 'should prepend header');
-				t.end();
-		  });
+	  process.chdir(dirPath);
+	  fs.copySync(inputFilePath, 'input.js');
+	  exec(uberLicensePath, function(execErr) {
+	  	if (execErr instanceof Error) {
+	  		throw execErr;
+	  	}
+			var inputFileContent = fs.readFileSync('input.js', 'utf8');
+			t.true(outputRegex.test(inputFileContent), 'should prepend header');
+			t.end();
 		});
 	});
 });
